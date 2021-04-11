c546833ebcd66326b6cab4bd8ce8369caafca666
manufedo <50418498+manufedo@users.noreply.github.com>
2021-04-11 01:59:54 -0300

    implementacion jarvis y judice

--- jarvis-judice/ejercicio.js
+++ jarvis-judice/ejercicio.js
@@ -37,53 +37,111 @@ function dither(image, factor)
 			var rComponent = image.data[index];
 			var gComponent = image.data[index + 1];
 			var bComponent = image.data[index + 2];
-    
+
 			var rClosestColor = getClosestColor(rComponent, factor);
 			var gClosestColor = getClosestColor(gComponent, factor);
 			var bClosestColor = getClosestColor(bComponent, factor);
 
-         	var rError = rComponent - rClosestColor;
-         	var gError = gComponent - gClosestColor;
-         	var bError = bComponent - bClosestColor;
+        var rError = rComponent - rClosestColor;
+        var gError = gComponent - gClosestColor;
+        var bError = bComponent - bClosestColor;
 
 			// saving real colors
-         	image.data[index]     = rClosestColor;                  
-         	image.data[index + 1] = gClosestColor;
+        image.data[index]     = rClosestColor;
+        image.data[index + 1] = gClosestColor;
 			image.data[index + 2] = bClosestColor;
 
+
 			// right neighbour
-         	if(column+1 < columnSize){ 	
-         		image.data[index + 4] += (rError*7)>>4;
-	         	image.data[index + 5] += (gError*7)>>4;
-				image.data[index + 6] += (bError*7)>>4;
-         	} 
+         	if(column+1 < columnSize){
+         		image.data[index + 4] += Math.floor(rError*7/48);
+	         	image.data[index + 5] += Math.floor(gError*7/48);
+						image.data[index + 6] += Math.floor(bError*7/48);
+         	}
+
+					// right right neighbour
+
+					if (column + 2 < columnSize){
+						image.data[index + 8] += Math.floor(rError*5/48);
+	         	image.data[index + 9] += Math.floor(gError*5/48);
+						image.data[index + 10] += Math.floor(bError*5/48);
+					}
 
          	if(row+1==rowSize) continue;
 
-         	// bottom left neighbour
-         	if(column > 0){
-         		image.data[index + columnSize - 4] += (rError*3)>>4;
-         		image.data[index + columnSize - 3] += (gError*3)>>4;
-         		image.data[index + columnSize - 2] += (bError*3)>>4;
-         	} 
+         	// bottom left left neighbour
+         	if(column > 2){
+         		image.data[index + columnSize - 8] += Math.floor(rError*3/48);
+         		image.data[index + columnSize - 7] += Math.floor(gError*3/48);
+         		image.data[index + columnSize - 6] += Math.floor(bError*3/48);
+         	}
+
+					// bottom left neighbour
+					if(column > 1){
+         		image.data[index + columnSize - 4] += Math.floor(rError*5/48);
+         		image.data[index + columnSize - 3] += Math.floor(gError*5/48);
+         		image.data[index + columnSize - 2] += Math.floor(bError*5/48);
+         	}
 
 			// bottom neighbour
-            image.data[index+columnSize    ] += (rError*5)>>4;
-            image.data[index+columnSize + 1] += (gError*5)>>4;
-            image.data[index+columnSize + 2] += (bError*5)>>4;
+            image.data[index+columnSize    ] += Math.floor(rError*7/48);
+            image.data[index+columnSize + 1] += Math.floor(gError*7/48);
+            image.data[index+columnSize + 2] += Math.floor(bError*7/48);
 
-			// bottom right neighbour
+					// bottom right neighbour
          	if(column+1 < columnSize){
-         	 	image.data[index+columnSize+4] += (rError*1)>>4;
-         	 	image.data[index+columnSize+5] += (gError*1)>>4;
-         	 	image.data[index+columnSize+6] += (bError*1)>>4;
+         	 	image.data[index+columnSize+4] += Math.floor(rError*5/48);
+         	 	image.data[index+columnSize+5] += Math.floor(gError*5/48);
+         	 	image.data[index+columnSize+6] += Math.floor(bError*5/48);
          	}
+
+					//bottom righ right neighbour
+					if(column+2 < columnSize){
+						image.data[index+columnSize+8] += Math.floor(rError*3/48);
+         	 	image.data[index+columnSize+9] += Math.floor(gError*3/48);
+         	 	image.data[index+columnSize+10] += Math.floor(bError*3/48);
+					}
+
+					if(row+2==rowSize) continue;
+
+					//bottom bottom left left neighbour
+					if(column > 2){
+         		image.data[index + columnSize*2 - 8] += Math.floor(rError*1/48);
+         		image.data[index + columnSize*2 - 7] += Math.floor(gError*1/48);
+         		image.data[index + columnSize*2 - 6] += Math.floor(bError*1/48);
+         	}
+
+					// bottom bottom left neighbour
+					if(column > 1){
+         		image.data[index + columnSize*2 - 4] += Math.floor(rError*3/48);
+         		image.data[index + columnSize*2 - 3] += Math.floor(gError*3/48);
+         		image.data[index + columnSize*2 - 2] += Math.floor(bError*3/48);
+         	}
+
+			// bottom bottom neighbour
+            image.data[index+columnSize*2    ] += Math.floor(rError*5/48);
+            image.data[index+columnSize*2 + 1] += Math.floor(gError*5/48);
+            image.data[index+columnSize*2 + 2] += Math.floor(bError*5/48);
+
+					// bottom bottom right neighbour
+         	if(column+1 < columnSize){
+         	 	image.data[index+columnSize*2+4] += Math.floor(rError*3/48);
+         	 	image.data[index+columnSize*2+5] += Math.floor(gError*3/48);
+         	 	image.data[index+columnSize*2+6] += Math.floor(bError*3/48);
+         	}
+
+					//bottom bottom righ right neighbour
+					if(column+2 < columnSize){
+						image.data[index+columnSize*2+8] += Math.floor(rError*1/48);
+         	 	image.data[index+columnSize*2+9] += Math.floor(gError*1/48);
+         	 	image.data[index+columnSize*2+10] += Math.floor(bError*1/48);
+					}
 		}
 	}
 }
 
 // Imágenes a restar (imageA y imageB) y el retorno en result
-function substraction(imageA,imageB,result) 
+function substraction(imageA,imageB,result)
 {
 	// testear bien esto - parece que anda?
 	var pixelSize = imageA.data.length;
@@ -94,8 +152,8 @@ function substraction(imageA,imageB,result)
 		gComponent = imageA.data[i * 4 + 1] - imageB.data[i * 4 + 1];
 		bComponent = imageA.data[i * 4 + 2] - imageB.data[i * 4 + 2];
 
-		result.data[i * 4]     = rComponent;
-		result.data[i * 4 + 1] = gComponent;
-		result.data[i * 4 + 2] = bComponent;
+		result.data[i * 4]     = Math.abs(rComponent);
+		result.data[i * 4 + 1] = Math.abs(gComponent);
+		result.data[i * 4 + 2] = Math.abs(bComponent);
 	}
 }
